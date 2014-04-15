5e4c9a3a21ba866f7408668620d17ad14901699c
Spurthi <talk2spurthi@gmail.com>
2014-04-15 17:16:50 -0400

    Ensemble Regressors AdaBoost, RFs

--- DecisionTreeEnsemble.py
+++ DecisionTreeEnsemble.py
@@ -1,4 +1,4 @@
-from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.ensemble import AdaBoostRegressor, RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 import numpy as np
@@ -7,35 +7,35 @@ import operator
 def RandomForest(xTrain, yTrain, xTest, yTest, treeNum):
 	rms = dict()
 	for trees in treeNum:
-		rf = RandomForestClassifier(n_estimators = trees)
+		rf = RandomForestRegressor(n_estimators = trees)
 		rf.fit(xTrain, yTrain)
 		yPred = rf.predict(xTest)
 		rms[trees] = sqrt(mean_squared_error(yTest, yPred))
 
-	(bestClassifier, rmse) = sorted(rms.iteritems(), key = operator.itemgetter(1))[0]
+	(bestRegressor, rmse) = sorted(rms.iteritems(), key = operator.itemgetter(1))[0]
 
-	return bestClassifier, rmse
+	return bestRegressor, rmse
 
 def AdaBoost(xTrain, yTrain, xTest, yTest, treeNum):
 	rms = dict()
 	for trees in treeNum:
-		ab = AdaBoostClassifier(n_estimators = trees)
+		ab = AdaBoostRegressor(n_estimators = trees)
 		ab.fit(xTrain, yTrain)
 		yPred = ab.predict(xTest)
 		rms[trees] = sqrt(mean_squared_error(yTest, yPred))
 
-	(bestClassifier, rmse) = sorted(rms.iteritems(), key = operator.itemgetter(1))[0]
+	(bestRegressor, rmse) = sorted(rms.iteritems(), key = operator.itemgetter(1))[0]
 
-	return bestClassifier, rmse
+	return bestRegressor, rmse
 
 def main():
 	
 	treeNum = list(xrange(1,502,10))
-	bestClassifier, rmse = RandomForest(xTrain, yTrain, xTest, yTest, treeNum)
-	print bestClassifier, rmse
+	bestRegressor, rmse = RandomForest(xTrain, yTrain, xTest, yTest, treeNum)
+	print bestRegressor, rmse
 
-	bestClassifier, rmse = AdaBoost(xTrain, yTrain, xTest, yTest, treeNum)
-	print bestClassifier, rmse
+	bestRegressor, rmse = AdaBoost(xTrain, yTrain, xTest, yTest, treeNum)
+	print bestRegressor, rmse
 
 if __name__=="__main__":
 	main()
