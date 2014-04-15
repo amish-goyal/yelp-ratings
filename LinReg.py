01fd5b238c811fa60c2423cdac184bc99297fc6c
Spurthi <spurthi@spurthi-HP-Pavilion-dv2500-Notebook-PC.(none)>
2014-04-15 06:06:10 -0400

    Modified again

--- LinReg.py
+++ LinReg.py
@@ -1,46 +1,50 @@
 from sklearn import linear_model
-#from sklearn import datasets
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 import numpy as np
+import operator
 
 def LinReg(xTrain, yTrain, xTest, yTest):
 	reg = linear_model.LinearRegression()
 	reg.fit(xTrain, yTrain)
-	accuracy = reg.score(xTest, yTest)
-	return accuracy
+	yPred = reg.predict(xTest)
+	rms = sqrt(mean_squared_error(yTest, yPred))
+	return rms
 
 def LinRegRidge(xTrain, yTrain, xTest, yTest, alphaList):
-	reg = linear_model.RidgeCV(alphas = alphaList, store_cv_values = True)
-	reg.fit(xTrain, yTrain)
-	#print reg.get_params, reg.alpha_
-	accuracy = reg.score(xTest, yTest)
-	return reg.alpha_, accuracy
+	rms = dict()
+	for alpha in alphas:
+		reg = linear_model.Ridge(alphas = alpha, store_cv_values = True)
+		reg.fit(xTrain, yTrain)
+		yPred = reg.predict(xTest)
+		rms[alpha] = sqrt(mean_squared_error(yTest, yPred))
+
+	(bestClassifier, rmse) = sorted(rms.iteritems(), key = operator.itemgetter(1))[0]
+	return bestClassifier, rmse
 
 def LinRegLasso(xTrain, yTrain, xTest, yTest, alphaList):
-	reg = linear_model.LassoCV(alphas = alphaList)
-	reg.fit(xTrain, yTrain)
-	#print reg.get_params, reg.alpha_
-	accuracy = reg.score(xTest, yTest)
-	return reg.alpha_, accuracy
+	rms = dict()
+	for alpha in alphas:
+		reg = linear_model.Lasso(alphas = alphaList)
+		reg.fit(xTrain, yTrain)
+		yPred = reg.predict(xTest)
+		rms[alpha] = sqrt(mean_squared_error(yTest, yPred))
+
+	(bestClassifier, rmse) = sorted(rms.iteritems(), key = operator.itemgetter(1))[0]
+	return bestClassifier, rmse
 
 def main():
-	'''diabetes = datasets.load_diabetes()
-	diabetes_X = diabetes.data[:, np.newaxis]
-	diabetes_X_temp = diabetes_X[:, :, 2]
-	xTrain = diabetes_X_temp[:-20]
-	xTest = diabetes_X_temp[-20:]
-	yTrain = diabetes.target[:-20]
-	yTest = diabetes.target[-20:]'''
-	
-	accuracy = LinReg(xTrain, yTrain, xTest, yTest)
-	print accuracy
+
+	rmse = LinReg(xTrain, yTrain, xTest, yTest)
+	print rmse
 	
 	alphaList = [10**x for x in xrange(-10,10,1)]
-	bestAlpha, accuracy = LinRegRidge(xTrain, yTrain, xTest, yTest, alphaList)
-	print bestAlpha, accuracy
+	bestAlpha, rmse = LinRegRidge(xTrain, yTrain, xTest, yTest, alphaList)
+	print bestAlpha, rmse
 
 	alphaList = [10**x for x in xrange(-30,1,1)]
-	bestAlpha, accuracy = LinRegLasso(xTrain, yTrain, xTest, yTest, alphaList)
-	print bestAlpha, accuracy
+	bestAlpha, rmse = LinRegLasso(xTrain, yTrain, xTest, yTest, alphaList)
+	print bestAlpha, rmse
 
 if __name__=="__main__":
 	main()
